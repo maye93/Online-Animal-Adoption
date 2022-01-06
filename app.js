edc30dfc21e7899d3f6368df82153d5c66b39533
maye93 <71053941+maye93@users.noreply.github.com>
2022-01-07 07:13:17 +0800

    added files

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-var song = Array("song1", "song2", "song3", "song4", "song5", "song6");
+// var song = Array("song1", "song2", "song3", "song4", "song5", "song6");
 
-function randomSong() {
-  var randomSong = song[Math.floor(Math.random() * song.length)];
-  document.getElementById('randomSong').value = randomSong;
+// function randomSong() {
+//   var randomSong = song[Math.floor(Math.random() * song.length)];
+//   document.getElementById('randomSong').value = randomSong;
+// }
+
+// var myArray = [
+//   "Apples",
+//   "Bananas",
+//   "Pears"
+// ];
+
+// function randomQuote() {
+//   var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+//   document.body.innerHTML = randomItem;
+// }
+
+var arr = []
+var imgArray = ["images/animals/cockatiels.png", "images/animals/fox.png"]
+
+function likeHeart() {
+    var img
+    arr.push("Heart")
+    console.log(arr)
+    if (arr.length < imgArray.length)
+        img = document.querySelector("#img").src = imgArray[(arr.length - 1) % imgArray.length]
+    else
+        img = document.querySelector("#img").src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
 }
 
-var myArray = [
-  "Apples",
-  "Bananas",
-  "Pears"
-];
-
-function randomQuote() {
-  var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-  document.body.innerHTML = randomItem;
+function Reject()
+{
+    arr.push("Reject")
+    console.log(arr)
+    if (arr.length < imgArray.length)
+        img = document.querySelector("#img").src = imgArray[(arr.length - 1) % imgArray.length]
+    else
+        img = document.querySelector("#img").src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
 }
