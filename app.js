7ceadce2b6dd99c8827bda1c526e42d3b093319a
maye93 <71053941+maye93@users.noreply.github.com>
2022-01-13 22:21:49 +0800

    problem is list

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var arr = []
+var counter = 0
+
 var animals = [
+  "images/animals/chinchilla.png",
   "images/animals/cockatiels.png", 
   "images/animals/fox.png", 
   "images/animals/glider.png", 
@@ -8,41 +11,60 @@ var animals = [
   "images/animals/racoon.png", 
   "images/animals/retriever.jpeg", 
   "images/animals/samoyed.png", 
-  "images/animals/toucan.png"]
-
-var namesArr = []
-var names = [
-  "chim",
-  "a",
-  "e",
-  "d",
-  "f",
-  "g",
-  "h",
-  "v",
-  "x"
+  "images/animals/toucan.png"
 ]
+var names = [
+  "Chim",
+  "Taffy",
+  "Flinch",
+  "Swifty",
+  "Hermes",
+  "Peanut Butter",
+  "Rufus",
+  "Goldie",
+  "Snickers",
+  "Truffle"
+]
+var ages = [
+  "5",
+  "8",
+  "2",
+  "6",
+  "4",
+  "1",
+  "3",
+  "10",
+  "9",
+  "7"
+]
+
+document.querySelector("#animal").src = animals[0]
+document.querySelector("#name").innerHTML = names[0]
+document.querySelector("#age").innerHTML = ages[0]
+
 function heart() {
-    var img
-    var h1
-    var h3
-    arr.push("Heart")
-    console.log(arr)
-    if (arr.length < 10) {
-      img = document.querySelector("#img").src = animals[(arr.length - 1) % animals.length]
-      h1 = document.querySelector("#h1").src = names[(nameArr.length - 1) % names.length]
-    } else {
-      document.querySelector(".heartReject").hidden = true
-    }    
+  arr.push("Heart")
+  console.log(arr)
+  console.log(animals[0])
+  if (arr.length <= animals.length) {
+    img = document.querySelector("#animal").src = animals[(arr.length - 1) % animals.length]
+    document.querySelector("#name").innerHTML = names[counter % names.length]
+    document.querySelector("#age").innerHTML = ages[counter % ages.length]
+    counter++
+  } else {
+    document.querySelector(".heartReject").hidden = true
+  }    
 }
 
 function reject() {
-    arr.push("Reject")
-    console.log(arr)
-    if (arr.length < 10) {
-      img = document.querySelector("#img").src = animals[(arr.length - 1) % animals.length]
-    } else {
-      document.querySelector(".heartReject").hidden = true
-    }   
+  arr.push("Reject")
+  console.log(arr)
+  if (arr.length <= animals.length) {
+    img = document.querySelector("#animal").src = animals[(arr.length - 1) % animals.length]
+    document.querySelector("#name").innerHTML = names[counter % names.length]
+    document.querySelector("#age").innerHTML = ages[counter % ages.length]
+    counter++
+  } else {
+    document.querySelector(".heartReject").hidden = true
+  }   
 }
-
